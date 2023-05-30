e2ede4cd0ea09531b3188a20e1e5bb8e91ad6ec1
YactayoC <syactayocrisostomo@gmail.com>
2023-05-30 00:07:06 -0500

    add connection db

--- config/connectionDB.js
+++ config/connectionDB.js
@@ -1,17 +1,22 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'oficial',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 });
 
-try {
-  await connection.connect();
-  console.log('Conexión exitosa a la base de datos MySQL');
-} catch (error) {
-  console.error('Error al conectar con la base de datos: ', error);
+async function handleConnect() {
+  try {
+    await connection.connect();
+    console.log('Conexión exitosa a la base de datos MySQL');
+  } catch (error) {
+    console.error('Error al conectar con la base de datos: ', error);
+  }
 }
 
-export default connection;
+export default handleConnect;
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
-import { routerAuth, routerProduct, routerBranch} from './routes/index.js';
+import { routerAuth, routerProduct } from './routes/index.js';
+import handleConnect from './config/connectionDB.js';
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', routerAuth);
 app.use('/api/product', routerProduct);
-app.use('/api/branch', routerBranch);
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+  await handleConnect();
   console.log(`Server is running on port 3000`);
 });
